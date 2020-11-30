9df92f5ec934cf428a48d51c6b831239c21eb786
Saladenicoise <jules.laptop@gmail.com>
2020-11-30 18:05:48 -0500

    Navbar detects wether or not you are logged in

--- navBar/navBar.php
+++ navBar/navBar.php
@@ -1,3 +1,26 @@
+<?php
+    /**
+    * @return bool
+    */
+    function is_session_started()
+    {
+        if ( php_sapi_name() !== 'cli' ) {
+            if ( version_compare(phpversion(), '5.4.0', '>=') ) {
+                return session_status() === PHP_SESSION_ACTIVE ? TRUE : FALSE;
+            } else {
+                return session_id() === '' ? FALSE : TRUE;
+            }
+        }
+        return FALSE;
+    }
+
+    if ( is_session_started() === FALSE ) session_start();
+    $replaceLogin = 0;
+    if(isset($_SESSION['login'])) {
+        $replaceLogin = 1;
+    }
+?>
+
 <ul class="navi">
      <!-- <li><a href="null">Shopping Page</a></li>
     <li><a href="null">Menu Page</a></li>
@@ -20,6 +43,11 @@
     <li class="navBar"><a class="navbarElement" href="../aboutUs/aboutUs.php">About Us</a></li>
     <li class="navBar"><a class="navbarElement" href="../contactUs/contactUs.php">Contact Us</a></li>
     <li class="navBar"><a class="navbarElement" href="../reviewPage/reviews.php">Reviews</a></li>
+    <?php
+    if($replaceLogin != 1) : ?>
     <li class="navBar" style="float: right;"><a class="navbarElement" href="../login/login.php">Login</a></li>
+    <?php else : ?>
+    <li class="navBar" style="float: right;"><a class="navbarElement" href="../regular.php">Profile</a></li>
+    <?php endif; ?>
     <li class="navBar" style="float: right;"><a class="navbarElement" href="../menu/cart.php">Cart</a></li>
 </ul>
